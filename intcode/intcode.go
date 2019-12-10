2d7cae09e457cba19285c382544d740a27eced72
Oleg Izgagin <37784140+oizgagin@users.noreply.github.com>
2019-12-11 01:39:08 +0300

    support relative mode in intcode

--- intcode/intcode.go
+++ intcode/intcode.go
@@ -47,13 +47,14 @@ func (mem Memory) Copy() Memory {
 
 type CPU struct {
 	ip     int
+	base   int
 	mem    Memory
 	input  func() int
 	output func(int)
 }
 
 func NewCPU(mem Memory, input func() int, output func(int)) *CPU {
-	return &CPU{ip: 0, mem: mem, input: input, output: output}
+	return &CPU{ip: 0, base: 0, mem: mem, input: input, output: output}
 }
 
 func (cpu *CPU) Memory() Memory {
@@ -63,28 +64,30 @@ func (cpu *CPU) Memory() Memory {
 type Opcode int
 
 const (
-	OpcodeAdd    Opcode = 1
-	OpcodeMul    Opcode = 2
-	OpcodeInput  Opcode = 3
-	OpcodeOutput Opcode = 4
-	OpcodeJt     Opcode = 5
-	OpcodeJf     Opcode = 6
-	OpcodeLt     Opcode = 7
-	OpcodeEq     Opcode = 8
-	OpcodeHalt   Opcode = 99
+	OpcodeAdd       Opcode = 1
+	OpcodeMul       Opcode = 2
+	OpcodeInput     Opcode = 3
+	OpcodeOutput    Opcode = 4
+	OpcodeJt        Opcode = 5
+	OpcodeJf        Opcode = 6
+	OpcodeLt        Opcode = 7
+	OpcodeEq        Opcode = 8
+	OpcodeAdjustRel Opcode = 9
+	OpcodeHalt      Opcode = 99
 )
 
 func (opcode Opcode) String() string {
 	strings := map[Opcode]string{
-		OpcodeAdd:    "ADD",
-		OpcodeMul:    "MUL",
-		OpcodeInput:  "INPUT",
-		OpcodeOutput: "OUTPUT",
-		OpcodeJt:     "JT",
-		OpcodeJf:     "JF",
-		OpcodeLt:     "LT",
-		OpcodeEq:     "EQ",
-		OpcodeHalt:   "HALT",
+		OpcodeAdd:       "ADD",
+		OpcodeMul:       "MUL",
+		OpcodeInput:     "INPUT",
+		OpcodeOutput:    "OUTPUT",
+		OpcodeJt:        "JT",
+		OpcodeJf:        "JF",
+		OpcodeLt:        "LT",
+		OpcodeEq:        "EQ",
+		OpcodeAdjustRel: "ADJUSTREL",
+		OpcodeHalt:      "HALT",
 	}
 	return strings[opcode]
 }
@@ -94,6 +97,7 @@ type ParamMode int
 const (
 	ModePosition  ParamMode = 0
 	ModeImmediate ParamMode = 1
+	ModeRelative  ParamMode = 2
 )
 
 type ParamModes int
@@ -133,6 +137,8 @@ func (cpu *CPU) Exec() CPUState {
 			cpu.execLt(modes)
 		case OpcodeEq:
 			cpu.execEq(modes)
+		case OpcodeAdjustRel:
+			cpu.execAdjustRel(modes)
 		case OpcodeHalt:
 			return CPUHalt
 		}
@@ -223,6 +229,12 @@ func (cpu *CPU) execEq(modes ParamModes) {
 	cpu.ip += 4
 }
 
+func (cpu *CPU) execAdjustRel(modes ParamModes) {
+	incr := cpu.param(cpu.ip+1, modes.Get(0))
+	cpu.base += incr
+	cpu.ip += 2
+}
+
 func (cpu *CPU) param(addr int, mode ParamMode) int {
 	v := cpu.mem.Get(addr)
 
@@ -231,6 +243,9 @@ func (cpu *CPU) param(addr int, mode ParamMode) int {
 	}
 	if mode == ModePosition {
 		return cpu.mem.Get(v)
+	}
+	if mode == ModeRelative {
+		return cpu.mem.Get(v + cpu.base)
 	}
 	panic(fmt.Sprintf("invalid mode: %v", mode))
 }
