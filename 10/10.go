93cbdb7a92ab3c64a2f36e09aee020a3ffed7d21
Oleg Izgagin <37784140+oizgagin@users.noreply.github.com>
2019-12-13 01:06:04 +0300

    add 10.1 solution

--- 10/10.go
+++ 10/10.go
@@ -13,6 +13,10 @@ func main() {
 	fmt.Println(solver.Solve())
 }
 
+func log(args ...interface{}) {
+	//fmt.Println(args...)
+}
+
 type point struct {
 	x, y int
 }
@@ -57,19 +61,26 @@ func solve1(v interface{}) interface{} {
 		return (x <= 0 && y <= 0) || (x > 0 && y > 0)
 	}
 
+	absi := func(i int) int {
+		if i < 0 {
+			return -i
+		}
+		return i
+	}
+
 	max := 0
 
 	for _, p := range m.points {
-		fmt.Println("POINT", p)
-		ps := make([]point, len(m.points)-1)
+		log("POINT", p)
+		ps := make([]point, len(m.points))
 		copy(ps, m.points)
 
 		blocked := make(map[point]bool)
-		visible := 1
+		visible := 0
 
 		sort.Slice(ps, func(i, j int) bool { return abs(p, ps[i]) < abs(p, ps[j]) })
 
-		for i, neigh := range ps {
+		for _, neigh := range ps {
 			if eq(p, neigh) {
 				continue
 			}
@@ -77,33 +88,63 @@ func solve1(v interface{}) interface{} {
 				continue
 			}
 
-			fmt.Println("\t", neigh, "IS VISIBLE")
+			log("\t", neigh, "IS VISIBLE")
 
 			visible++
 
 			dx, dy := neigh.x-p.x, neigh.y-p.y
 
-			for j := i + 1; j < len(ps); j++ {
+			for j := 0; j < len(ps); j++ {
+				if eq(neigh, ps[j]) {
+					continue
+				}
+
 				ddx, ddy := ps[j].x-p.x, ps[j].y-p.y
+				log("\t\t", ps[j], "CHECK", neigh, "dx", dx, "dy", dy, "ddx", ddx, "ddy", ddy)
 
 				if !sign(dx, ddx) {
+					log("\t\t\tNOT SIGN", "dx", dx, "ddx", ddx)
 					continue
 				}
 				if !sign(dy, ddy) {
+					log("\t\t\tNOT SIGN", "dy", dx, "ddy", ddx)
 					continue
 				}
 				if dx == 0 && ddx != 0 || dx != 0 && ddx == 0 {
+					log("\t\t\tNOT ZERO", "dx", dx, "ddx", ddx)
 					continue
 				}
 				if dy == 0 && ddy != 0 || dy != 0 && ddy == 0 {
+					log("\t\t\tNOT ZERO", "dy", dy, "ddy", ddy)
 					continue
 				}
-				if (dx == 0 && ddx == 0 || ddx%dx == 0) && (dy == 0 && ddy == 0 || ddy%dy == 0) {
-					fmt.Println("\t\t", ps[j], "IS BLOCKED BY", neigh)
+				if dx == 0 && ddx == 0 {
+					if absi(dy) > absi(ddy) {
+						log("\t\t\t", "dy > ddy", "dy", dy, "ddy", ddy)
+						continue
+					}
+					log("\t\t\t", ps[j], "IS BLOCKED BY DX ZERO", neigh, "dx", dx, "dy", dy, "ddx", ddx, "ddy")
 					blocked[ps[j]] = true
+					continue
 				}
+				if dy == 0 && ddy == 0 {
+					if absi(dx) > absi(ddx) {
+						log("\t\t\t", "dx > ddx", "dx", dx, "ddx", ddx)
+						continue
+					}
+					log("\t\t\t", ps[j], "IS BLOCKED BY DY ZERO", neigh, "dx", dx, "dy", dy, "ddx", ddx, "ddy", ddy)
+					blocked[ps[j]] = true
+					continue
+				}
+				if dx != 0 && dy != 0 && dy*ddx != dx*ddy {
+					continue
+				}
+				log("\t\t\t", ps[j], "IS BLOCKED BY", neigh, "dx", dx, "dy", dy, "ddx", ddx, "ddy", ddy, "ddx/dx", ddx/dx, "ddy/dy", ddy/dy)
+				blocked[ps[j]] = true
 			}
 		}
+
+		log("VISIBLE", visible)
 
 		if visible > max {
 			max = visible
