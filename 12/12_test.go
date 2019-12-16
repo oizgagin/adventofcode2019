9780381f13f60cd467b476219ac744f47006bb09
Oleg Izgagin <37784140+oizgagin@users.noreply.github.com>
2019-12-16 14:51:52 +0300

    add 12.2 tests

--- 12/12_test.go
+++ 12/12_test.go
@@ -2,7 +2,7 @@ package main
 
 import "testing"
 
-func TestSolve1(t *testing.T) {
+func TestTick(t *testing.T) {
 
 	testCases := []struct {
 		coords string
@@ -38,6 +38,40 @@ func TestSolve1(t *testing.T) {
 		}
 
 		if got := system.Energy(); got != tc.want {
+			t.Fatalf("#%d: got %d, want %d", i, got, tc.want)
+		}
+	}
+
+}
+
+func TestSolve2(t *testing.T) {
+
+	testCases := []struct {
+		coords string
+		want   int
+	}{
+		{
+			coords: `
+				<x=-1, y=0, z=2>
+				<x=2, y=-10, z=-7>
+				<x=4, y=-8, z=8>
+				<x=3, y=5, z=-1>
+			`,
+			want: 2772,
+		},
+		{
+			coords: `
+				<x=-8, y=-10, z=0>
+				<x=5, y=5, z=10>
+				<x=2, y=-7, z=3>
+				<x=9, y=-8, z=-3>
+			`,
+			want: 4686774924,
+		},
+	}
+
+	for i, tc := range testCases {
+		if got := solve2(parseCoords(tc.coords)); got != tc.want {
 			t.Fatalf("#%d: got %d, want %d", i, got, tc.want)
 		}
 	}
